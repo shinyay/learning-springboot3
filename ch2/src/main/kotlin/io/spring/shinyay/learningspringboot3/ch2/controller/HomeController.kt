8b63aa1c1b132bf30405856875bce640f68dd990
shinyay <shinya.com@gmail.com>
2023-02-13 18:15:16 +0900

    Add mapping function

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
@@ -1,7 +1,13 @@
 package io.spring.shinyay.learningspringboot3.ch2.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class HomeController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
 }
