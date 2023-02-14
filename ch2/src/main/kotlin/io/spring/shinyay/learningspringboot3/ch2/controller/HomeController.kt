ee0a4498ab9320d4bac6e77b65c6e377767ec35f
shinyay <shinya.com@gmail.com>
2023-02-14 19:33:16 +0900

    Add mutableList

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class HomeController {
 
+    val videos = mutableListOf<Video>(
+        Video("Spring Boot 3 in Action"),
+        Video("Spring Framework 6 in Action"),
+        Video("Spring Cloud in Action")
+    )
+
     @GetMapping("/")
     fun index(): String {
         return "index"
