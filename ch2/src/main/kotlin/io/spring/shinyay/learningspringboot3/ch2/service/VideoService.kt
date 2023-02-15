a1ed920ee72a4e9e35be79db428e7c81a5b9bf35
shinyay <shinya.com@gmail.com>
2023-02-15 19:18:16 +0900

    Add mutableList in Service layer

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/service/VideoService.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/service/VideoService.kt
@@ -1,7 +1,12 @@
 package io.spring.shinyay.learningspringboot3.ch2.service
 
+import io.spring.shinyay.learningspringboot3.ch2.entity.Video
 import org.springframework.stereotype.Service
 
 @Service
 class VideoService {
+
+    val videos: List<Video> = mutableListOf(
+        Video("Learning Spring Boot 3")
+    )
 }
