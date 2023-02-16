15d773bf539bec6957950e7e6425de8465545423
shinyay <shinya.com@gmail.com>
2023-02-16 17:44:52 +0900

    Add videoservice injection

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/HomeController.kt
@@ -1,18 +1,13 @@
 package io.spring.shinyay.learningspringboot3.ch2.controller
 
 import io.spring.shinyay.learningspringboot3.ch2.entity.Video
+import io.spring.shinyay.learningspringboot3.ch2.service.VideoService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HomeController {
-
-    val videos = mutableListOf<Video>(
-        Video("Spring Boot 3 in Action"),
-        Video("Spring Framework 6 in Action"),
-        Video("Spring Cloud in Action")
-    )
+class HomeController(val videoService: VideoService) {
 
     @GetMapping("/")
     fun index(model: Model): String {
