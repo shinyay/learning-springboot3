0bb77c6fac71b1854f5677d3b06c86cca2b6eaf3
shinyay <shinya.com@gmail.com>
2023-02-16 19:20:43 +0900

    Add create video function with Post METHOD

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
@@ -3,6 +3,8 @@ package io.spring.shinyay.learningspringboot3.ch2.controller
 import io.spring.shinyay.learningspringboot3.ch2.entity.Video
 import io.spring.shinyay.learningspringboot3.ch2.service.VideoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,4 +14,7 @@ class ApiController(val videoService: VideoService) {
 
     @GetMapping("/videos")
     fun all(): List<Video> = videoService.getVideos()
+
+    @PostMapping("videos")
+    fun newVideo(@RequestBody newVideo: Video) = videoService.create(newVideo)
 }
