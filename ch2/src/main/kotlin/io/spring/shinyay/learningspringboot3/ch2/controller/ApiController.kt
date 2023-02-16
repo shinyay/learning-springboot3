01ae4ccdbb44e888cf80a0610b1a45e6e1adae3a
shinyay <shinya.com@gmail.com>
2023-02-16 19:02:44 +0900

    Add mapping path

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
@@ -2,10 +2,12 @@ package io.spring.shinyay.learningspringboot3.ch2.controller
 
 import io.spring.shinyay.learningspringboot3.ch2.entity.Video
 import io.spring.shinyay.learningspringboot3.ch2.service.VideoService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ApiController(val videoService: VideoService) {
 
+    @GetMapping("/api/v1/videos")
     fun all(): List<Video> = videoService.getVideos()
 }
