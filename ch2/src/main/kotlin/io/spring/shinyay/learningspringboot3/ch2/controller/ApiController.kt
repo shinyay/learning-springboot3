4293ea895ac4958e3257eb358c04aa7e1e6cd9df
shinyay <shinya.com@gmail.com>
2023-02-16 18:59:32 +0900

    Add a function to retlieve all video data

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
@@ -1,8 +1,11 @@
 package io.spring.shinyay.learningspringboot3.ch2.controller
 
+import io.spring.shinyay.learningspringboot3.ch2.entity.Video
 import io.spring.shinyay.learningspringboot3.ch2.service.VideoService
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ApiController(val videoService: VideoService) {
+
+    fun all(): List<Video> = videoService.getVideos()
 }
