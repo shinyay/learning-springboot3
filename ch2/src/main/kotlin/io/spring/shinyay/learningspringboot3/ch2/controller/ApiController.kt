ff70271bbf5380d4ef4303df36cf82af059afc96
shinyay <shinya.com@gmail.com>
2023-02-16 18:58:20 +0900

    Add costructor injection for Video Service

--- ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
+++ ch2/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch2/controller/ApiController.kt
@@ -1,7 +1,8 @@
 package io.spring.shinyay.learningspringboot3.ch2.controller
 
+import io.spring.shinyay.learningspringboot3.ch2.service.VideoService
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ApiController {
+class ApiController(val videoService: VideoService) {
 }
