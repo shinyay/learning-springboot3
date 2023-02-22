61c39e027985c5b10f23d0691c2c8b406abe36cf
shinyay <shinya.com@gmail.com>
2023-02-22 16:07:49 +0900

    Modify typo

--- ch2/src/main/javascript/App.js
+++ ch2/src/main/javascript/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ListOfVideos from './ListOfVideos'
-import NewVideo from './NewVide'
+import NewVideo from './NewVideo'
 
 export function App() {
     return (
         <div>
             <ListOfVideos/>
+            <NewVideo/>
         </div>
     )
 }
