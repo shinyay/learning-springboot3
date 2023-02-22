c3cbbc90b73716e8c3146fbc26b93870a682ce74
shinyay <shinya.com@gmail.com>
2023-02-22 15:37:51 +0900

    Add function to invoke fetching by clicking UI

--- ch2/src/main/javascript/NewVideo.js
+++ ch2/src/main/javascript/NewVideo.js
@@ -5,9 +5,14 @@ class NewVideo extends React.Component {
         super(props)
         this.state = {name: ""}
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(event) {
         this.setState({name: event.target.value})
+    }
+
+    async handleSubmit(event) {
+
     }
 }
