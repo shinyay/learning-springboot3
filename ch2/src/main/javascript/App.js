26f52d295f72ccd55fece2544e55378babc7bbcc
shinyay <shinya.com@gmail.com>
2023-02-22 14:42:31 +0900

    Add logic to impopr other sources

---
+++ ch2/src/main/javascript/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ListOfVideos from './ListOfVideos'
+import NewVideo from './NewVide'
+
+export function App() {
+    return (
+        <div>
+            <ListOfVideos/>
+        </div>
+    )
+}
