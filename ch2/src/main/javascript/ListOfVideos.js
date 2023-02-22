f9d7a23e63cc2c6fd4747d703bb606abd3a4c54f
shinyay <shinya.com@gmail.com>
2023-02-22 16:50:37 +0900

    Modify typo

--- ch2/src/main/javascript/ListOfVideos.js
+++ ch2/src/main/javascript/ListOfVideos.js
@@ -2,12 +2,12 @@ import React from "react"
 
 class ListOfVideos extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {data: []}
     }
 
     async componentDidMount() {
-        let json = (await fetch("/api/v1/videos")).json()
+        let json = await fetch("/api/v1/videos").then(r => r.json())
         this.setState({data: json})
     }
 
