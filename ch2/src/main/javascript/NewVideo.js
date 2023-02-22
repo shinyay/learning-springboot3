ef4cda36aa8452e49681939a0a759060a4dbd22d
shinyay <shinya.com@gmail.com>
2023-02-22 15:48:32 +0900

    Add redering form

--- ch2/src/main/javascript/NewVideo.js
+++ ch2/src/main/javascript/NewVideo.js
@@ -24,4 +24,15 @@ class NewVideo extends React.Component {
         }).then(response =>
             window.location.href = "/react")
     }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type=text
+                       value={this.state.name}
+                       onChange={this.handleChange}/>
+                <button type={"submit"}>Submit</button>
+            </form>
+        )
+    }
 }
