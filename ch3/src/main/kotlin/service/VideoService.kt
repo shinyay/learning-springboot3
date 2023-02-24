f4e3752a0ebeef8c43e788efb415f720f4db6889
shinyay <shinya.com@gmail.com>
2023-02-24 16:54:39 +0900

    Add service logic which created in Chapter 2

--- ch3/src/main/kotlin/service/VideoService.kt
+++ ch3/src/main/kotlin/service/VideoService.kt
@@ -1,4 +1,21 @@
 package service
 
+import org.springframework.stereotype.Service
+
+@Service
 class VideoService {
+
+    private var videos: MutableList<Video> = mutableListOf(
+        Video("Learning Spring Boot 3"),
+        Video("Spring Boot 3 in Action"),
+        Video("Spring Framework 6 in Action"),
+        Video("Spring Cloud in Action"),
+    )
+
+    fun getVideos(): List<Video> = videos
+
+    fun create(newVideo: Video): Video? {
+        videos.add(newVideo)
+        return newVideo
+    }
 }
