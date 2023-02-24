d446420dcb75c68cd0144e63b8f4c960a90cefe7
shinyay <shinya.com@gmail.com>
2023-02-24 16:52:33 +0900

    Add logic created in Chapter 2

--- ch3/src/main/kotlin/controller/HomeController.kt
+++ ch3/src/main/kotlin/controller/HomeController.kt
@@ -1,4 +1,23 @@
 package controller
 
-class HomeController {
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller
+class HomeController(val videoService: VideoService) {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("videos", videoService.getVideos())
+        return "index"
+    }
+
+    @PostMapping("/new-video")
+    fun newVideo(@ModelAttribute video: Video): String {
+        videoService.create(video)
+        return "redirect:/"
+    }
 }
