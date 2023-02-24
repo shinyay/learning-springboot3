15be929bfa19b6daaf3e4439c0673a8e03e2b583
shinyay <shinya.com@gmail.com>
2023-02-24 16:19:59 +0900

    Add custom function to find entities by Name

--- ch3/src/main/kotlin/repository/VideoRepository.kt
+++ ch3/src/main/kotlin/repository/VideoRepository.kt
@@ -3,4 +3,6 @@ package repository
 import entity.VideoEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface VideoRepository : JpaRepository<VideoEntity?, Long?>
+interface VideoRepository : JpaRepository<VideoEntity?, Long?> {
+    fun findByName(name: String): List<VideoEntity>
+}
