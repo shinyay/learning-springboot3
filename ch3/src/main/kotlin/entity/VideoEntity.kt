97eff0b8fe8698f8e317d4d8b3300e591f77454b
shinyay <shinya.com@gmail.com>
2023-02-24 15:07:23 +0900

    Add annotations for Primaryley

--- ch3/src/main/kotlin/entity/VideoEntity.kt
+++ ch3/src/main/kotlin/entity/VideoEntity.kt
@@ -1,9 +1,14 @@
 package entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
 
 @Entity
 class VideoEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
     val name: String,
     val description: String) {
