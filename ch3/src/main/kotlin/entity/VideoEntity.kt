ab1ab4397407ac4904b0b6b288ee3f1efdbda306
shinyay <shinya.com@gmail.com>
2023-02-24 15:59:15 +0900

    Modify vidsibility as Public

--- ch3/src/main/kotlin/entity/VideoEntity.kt
+++ ch3/src/main/kotlin/entity/VideoEntity.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.Id
 
 
 @Entity
-internal class VideoEntity(var name: String?, var description: String?) {
+class VideoEntity(var name: String?, var description: String?) {
     @Id
     @GeneratedValue
     var id: Long? = null
