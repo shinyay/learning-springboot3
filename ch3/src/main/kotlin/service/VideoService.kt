536a398eff49a4ba368e42e7e8f9753cea87cde8
shinyay <shinya.com@gmail.com>
2023-02-25 18:30:10 +0900

    Add VideoRepository

--- ch3/src/main/kotlin/service/VideoService.kt
+++ ch3/src/main/kotlin/service/VideoService.kt
@@ -4,9 +4,11 @@ import dto.VideoSearch
 import entity.Video
 import entity.VideoEntity
 import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
+import repository.VideoRepository
 
 @Service
-class VideoService {
+class VideoService(val repository: VideoRepository) {
 
     private var videos: MutableList<Video> = mutableListOf(
         Video("Learning Spring Boot 3", ""),
@@ -22,5 +24,5 @@ class VideoService {
         return newVideo
     }
 
-    fun search(videoSearch: VideoSearch): List<VideoEntity>? {return null}
+
 }
