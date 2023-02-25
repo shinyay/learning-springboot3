3ef92bb8d446d2fb97e43e0afbfd9165de860535
shinyay <shinya.com@gmail.com>
2023-02-25 17:37:21 +0900

    Modify import declaration for Video entity

--- ch3/src/main/kotlin/service/VideoService.kt
+++ ch3/src/main/kotlin/service/VideoService.kt
@@ -1,15 +1,16 @@
 package service
 
+import entity.Video
 import org.springframework.stereotype.Service
 
 @Service
 class VideoService {
 
     private var videos: MutableList<Video> = mutableListOf(
-        Video("Learning Spring Boot 3"),
-        Video("Spring Boot 3 in Action"),
-        Video("Spring Framework 6 in Action"),
-        Video("Spring Cloud in Action"),
+        Video("Learning Spring Boot 3", ""),
+        Video("Spring Boot 3 in Action", ""),
+        Video("Spring Framework 6 in Action", ""),
+        Video("Spring Cloud in Action", ""),
     )
 
     fun getVideos(): List<Video> = videos
