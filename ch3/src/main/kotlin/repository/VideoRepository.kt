4352e726af197736ff74308995ef272c75c28460
shinyay <shinya.com@gmail.com>
2023-02-27 14:53:26 +0900

    Add find By Name Contains Or Description Contains AllIgnoreCase

--- ch3/src/main/kotlin/repository/VideoRepository.kt
+++ ch3/src/main/kotlin/repository/VideoRepository.kt
@@ -7,4 +7,8 @@ interface VideoRepository : JpaRepository<VideoEntity?, Long?> {
     fun findByName(name: String): List<VideoEntity>
 
     fun findByNameContainsIgnoreCase(partialName: String)
+
+    fun findByDescriptionContainsIgnoreCase(partialDescription:String)
+
+    fun findByNameContainsOrDescriptionContainsAllIgnoreCase(partialName: String, partialDescription: String)
 }
