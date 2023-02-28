23f7e4c9e2b33f8681bbf40a8f75dc89104a7bc1
shinyay <shinya.com@gmail.com>
2023-02-28 18:26:19 +0900

    Add chapter 4 project template for Gradle

---
+++ ch4/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch4/Chapter4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.spring.shinyay.learningspringboot3.ch4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
