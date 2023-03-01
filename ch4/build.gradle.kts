e5ccce0d4935b725d843a3bfc5be61a4c92662de
shinyay <shinya.com@gmail.com>
2023-03-01 19:14:32 +0900

    Add dependency for Mustache

--- ch4/build.gradle.kts
+++ ch4/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
