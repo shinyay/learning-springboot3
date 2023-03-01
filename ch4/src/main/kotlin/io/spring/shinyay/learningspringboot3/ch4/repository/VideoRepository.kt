17d94f47e57bab962ca127321e00a6bc8bdb41a9
shinyay <shinya.com@gmail.com>
2023-03-01 19:37:34 +0900

    Add Repository for Video Entity operation

---
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/repository/VideoRepository.kt
@@ -0,0 +1,16 @@
+package io.spring.shinyay.learningspringboot3.ch3.repository
+
+import io.spring.shinyay.learningspringboot3.ch3.entity.VideoEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface VideoRepository : JpaRepository<VideoEntity?, Long?> {
+    fun findByName(name: String): List<VideoEntity>
+
+    fun findByNameContainsIgnoreCase(partialName: String): List<VideoEntity>
+
+    fun findByDescriptionContainsIgnoreCase(partialDescription:String): List<VideoEntity>
+
+    fun findByNameContainsOrDescriptionContainsAllIgnoreCase(
+        partialName: String,
+        partialDescription: String): List<VideoEntity>
+}
