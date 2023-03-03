64ad1ccadf8235eaab9cebcb493c1344df49a86b
shinyay <shinya.com@gmail.com>
2023-03-03 09:46:07 +0900

    Add funtion signature for userDetailsService

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
@@ -1,7 +1,13 @@
 package io.spring.shinyay.learningspringboot3.ch4.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    fun userDetailsService() {
+        
+    }
 }
