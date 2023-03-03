7c8e54f8f2978be2f60e45f3cf2e66559a97fca8
shinyay <shinya.com@gmail.com>
2023-03-03 10:30:50 +0900

    Add secondary constructor

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
@@ -1,11 +1,19 @@
 package io.spring.shinyay.learningspringboot3.ch4.security.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
 
 @Entity
-class UserAccount {
+class UserAccount(
+    val username: String?,
+    val password: String?,
+    @ElementCollection(fetch = FetchType.EAGER)
+    val authorities: List<GrantedAuthority>?
+) {
+    constructor() : this("", "", null) {
+
+    }
+
     @Id
     @GeneratedValue
     val id: Long? = null
