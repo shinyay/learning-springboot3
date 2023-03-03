6ba552a537c3e2ff0c02abc555f1906edfb42738
shinyay <shinya.com@gmail.com>
2023-03-03 09:51:27 +0900

    Add general user and admin user with using withDefaultPasswordEncoder

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
@@ -2,12 +2,31 @@ package io.spring.shinyay.learningspringboot3.ch4.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun userDetailsService() {
-
+    fun userDetailsService(): UserDetailsService {
+        val userDetailsManager: UserDetailsManager = InMemoryUserDetailsManager()
+        userDetailsManager.createUser(
+            User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build()
+        )
+        userDetailsManager.createUser(
+            User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build()
+        )
+        return userDetailsManager
     }
 }
