42a4df1b780e657d5c357d699bf484824068541f
shinyay <shinya.com@gmail.com>
2023-03-03 10:21:31 +0900

    Add id for primary key

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
@@ -1,4 +1,12 @@
 package io.spring.shinyay.learningspringboot3.ch4.security.entity
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity
 class UserAccount {
+    @Id
+    @GeneratedValue
+    val id: Long? = null
 }
