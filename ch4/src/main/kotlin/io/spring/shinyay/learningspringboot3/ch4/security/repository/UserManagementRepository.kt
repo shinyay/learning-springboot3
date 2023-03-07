e6f4a051e5c6c638ab10068a1ef735e9dc890754
shinyay <shinya.com@gmail.com>
2023-03-07 18:50:38 +0900

    Modify class to interface

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserManagementRepository.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserManagementRepository.kt
@@ -1,6 +1,7 @@
 package io.spring.shinyay.learningspringboot3.ch4.security.repository
 
+import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserManagementRepository : JpaRepository {
+interface UserManagementRepository : JpaRepository<UserAccount, Long> {
 }
