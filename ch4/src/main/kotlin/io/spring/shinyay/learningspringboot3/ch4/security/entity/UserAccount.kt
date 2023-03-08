83a75cc54c280c41e936125f37ecd9c9278f1148
shinyay <shinya.com@gmail.com>
2023-03-08 19:22:04 +0900

    Add entity for user account

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
@@ -2,19 +2,37 @@ package io.spring.shinyay.learningspringboot3.ch4.security.entity
 
 import jakarta.persistence.*
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.*
+
 
 @Entity
-class UserAccount(
-    val username: String?,
-    val password: String?,
-    @ElementCollection(fetch = FetchType.EAGER)
-    val authorities: List<GrantedAuthority>?
-) {
-    constructor() : this("", "", null) {
-
-    }
-
+class UserAccount {
     @Id
     @GeneratedValue
     val id: Long? = null
+    val username: String? = null
+    val password: String? = null
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private val authorities: MutableList<GrantedAuthority>? = mutableListOf()
+
+    protected constructor()
+    constructor(username: String?, password: String?, vararg authorities: String) {
+        this.username = username
+        this.password = password
+        this.authorities = Arrays.stream(authorities) //
+            .map { role: String? ->
+                SimpleGrantedAuthority(
+                    role
+                )
+            } //
+            .map { obj: SimpleGrantedAuthority? ->
+                GrantedAuthority::class.java.cast(
+                    obj
+                )
+            } //
+            .toList()
+    }
 }
