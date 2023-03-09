81b1d01ad081e56a4fd731c6430b8719c0ab6df9
shinyay <shinya.com@gmail.com>
2023-03-09 11:39:04 +0900

    Modify userdetailservice

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
@@ -2,6 +2,7 @@ package io.spring.shinyay.learningspringboot3.ch4.security
 
 import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
 import io.spring.shinyay.learningspringboot3.ch4.security.repository.UserManagementRepository
+import io.spring.shinyay.learningspringboot3.ch4.security.repository.UserRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,22 +24,29 @@ class SecurityConfig {
     }
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        val userDetailsManager: UserDetailsManager = InMemoryUserDetailsManager()
-        userDetailsManager.createUser(
-            User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build()
-        )
-        userDetailsManager.createUser(
-            User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN")
-                .build()
-        )
-        return userDetailsManager
+    fun userDetailsService(repository: UserRepository): UserDetailsService {
+        return UserDetailsService {
+            repository.findByUsername(it).asUser()
+        }
     }
+
+//    @Bean
+//    fun userDetailsService(): UserDetailsService {
+//        val userDetailsManager: UserDetailsManager = InMemoryUserDetailsManager()
+//        userDetailsManager.createUser(
+//            User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build()
+//        )
+//        userDetailsManager.createUser(
+//            User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("password")
+//                .roles("ADMIN")
+//                .build()
+//        )
+//        return userDetailsManager
+//    }
 }
