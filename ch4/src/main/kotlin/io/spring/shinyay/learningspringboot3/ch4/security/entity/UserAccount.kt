d7c81c71cfbb021dba7f14c9cd2f562feebae279
shinyay <shinya.com@gmail.com>
2023-03-09 11:10:10 +0900

    Add constructor for authorities list

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/entity/UserAccount.kt
@@ -3,18 +3,27 @@ package io.spring.shinyay.learningspringboot3.ch4.security.entity
 import jakarta.persistence.*
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
-import java.util.*
 
 
 @Entity
-class UserAccount {
+class UserAccount() {
+
     @Id
     @GeneratedValue
-    val id: Long? = null
-    val username: String? = null
-    val password: String? = null
+    var id: Long? = null
+    var username: String? = null
+    var password: String? = null
 
     @ElementCollection(fetch = FetchType.EAGER)
-    val authorities: MutableList<GrantedAuthority> = mutableListOf()
+    var authorities: List<GrantedAuthority> = mutableListOf()
+
+    constructor(username: String, password: String, vararg authorities: String)
+            : this() {
+        this.username = username
+        this.password = password
+        this.authorities = authorities
+            .map(::SimpleGrantedAuthority)
+            .map { it as GrantedAuthority }
+            .toList()
+    }
 }
