39c085e4607f6ff5fb6dd929a9a0ed3189aa72c9
shinyay <shinya.com@gmail.com>
2023-03-09 11:15:39 +0900

    Add custom finder to find acccount by user name

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
@@ -4,5 +4,6 @@ import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
 import org.springframework.data.repository.Repository
 
 interface UserRepository : Repository<UserAccount, Long> {
-    
+
+    fun findByUsername(username: String): UserAccount
 }
