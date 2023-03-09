8165b5291da05e10fe805823147367c00e2ca55a
shinyay <shinya.com@gmail.com>
2023-03-09 11:04:19 +0900

    Add generating test data with CommandLineRunner

--- ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
+++ ch4/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/SecurityConfig.kt
@@ -1,5 +1,8 @@
 package io.spring.shinyay.learningspringboot3.ch4.security
 
+import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
+import io.spring.shinyay.learningspringboot3.ch4.security.repository.UserManagementRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.User
@@ -7,8 +10,17 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
 
+
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    fun initUsers(repository: UserManagementRepository): CommandLineRunner? {
+        return CommandLineRunner {
+            repository.save(UserAccount("user", "password", "ROLE_USER"))
+            repository.save(UserAccount("admin", "password", "ROLE_ADMIN"))
+        }
+    }
 
     @Bean
     fun userDetailsService(): UserDetailsService {
