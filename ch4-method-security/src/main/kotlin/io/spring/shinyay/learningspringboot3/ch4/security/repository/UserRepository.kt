c0ceb7d4240b2f8baeb38e93b113cf509b716b7f
shinyay <shinya.com@gmail.com>
2023-03-12 11:28:29 +0900

    Add override annnotation

--- ch4-method-security/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
+++ ch4-method-security/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
@@ -10,5 +10,6 @@ interface UserRepository : Repository<UserAccount, Long> {
     fun findByUsername(username: String): UserAccount
 
     @PreAuthorize("#entity.username == authentication.name")
+    @Override
     fun delete(entity: VideoEntity)
 }
