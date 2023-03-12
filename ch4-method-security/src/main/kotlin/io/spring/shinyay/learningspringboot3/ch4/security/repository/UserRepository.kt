0e255defd3b67fff24952c53663749affbc21b62
shinyay <shinya.com@gmail.com>
2023-03-12 10:49:46 +0900

    Add delete function with PreAuthorized annotation for curitomizing security check

--- ch4-method-security/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
+++ ch4-method-security/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
@@ -1,9 +1,14 @@
 package io.spring.shinyay.learningspringboot3.ch4.security.repository
 
 import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
+import io.spring.shinyay.learningspringboot3.ch4.video.entity.VideoEntity
 import org.springframework.data.repository.Repository
+import org.springframework.security.access.prepost.PreAuthorize
 
 interface UserRepository : Repository<UserAccount, Long> {
 
     fun findByUsername(username: String): UserAccount
+
+    @PreAuthorize("#entity.username == authentication.name")
+    fun delete(entity: VideoEntity)
 }
