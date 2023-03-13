d0e20b188d50e49f4ca8151880a870b460373164
shinyay <shinya.com@gmail.com>
2023-03-13 13:50:48 +0900

    Add static contant variance

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/YouTubeConfig.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/YouTubeConfig.kt
@@ -4,4 +4,8 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class YouTubeConfig {
+
+    companion object {
+        const val YOUTUBE_V3_API = "https://www.googleapis.com/youtube/v3"
+    }
 }
