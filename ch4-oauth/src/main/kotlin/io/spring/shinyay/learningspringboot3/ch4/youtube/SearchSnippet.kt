4bb4d24e4f052add8449c09c7efb1c8ccf224594
shinyay <shinya.com@gmail.com>
2023-03-13 16:09:18 +0900

    Add Search snippet

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchSnippet.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchSnippet.kt
@@ -1,3 +1,10 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube
 
-data class SearchSnippet()
+data class SearchSnippet(
+    val publishedAt: String,
+    val channelId: String,
+    val title: String,
+    val description: String,
+    val thumbnails: Map<String, SearchThumbnail>,
+    val channelTitle: String
+    )
