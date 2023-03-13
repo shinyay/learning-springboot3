91107a3fb09485b672868be189edf612e63c0570
shinyay <shinya.com@gmail.com>
2023-03-13 16:17:39 +0900

    Add srcurity components

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserManagementRepository.kt
+++
@@ -1,7 +0,0 @@
-package io.spring.shinyay.learningspringboot3.ch4.security.repository
-
-import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserManagementRepository : JpaRepository<UserAccount, Long> {
-}
--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package io.spring.shinyay.learningspringboot3.ch4.security.repository
-
-import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
-import org.springframework.data.repository.Repository
-
-interface UserRepository : Repository<UserAccount, Long> {
-
-    fun findByUsername(username: String): UserAccount
-}
