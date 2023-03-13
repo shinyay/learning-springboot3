1a5426b4f57fc93e4f8b5374e333948ffda567bc
shinyay <shinya.com@gmail.com>
2023-03-13 15:56:55 +0900

    Modify syntax

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchId.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchId.kt
@@ -1,3 +1,6 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube
 
-data class SearchId(val kind: String, val videoId: String, val channelId: String, val playlistId: String)
+data class SearchId(val kind: String,
+                    val videoId: String,
+                    val channelId: String,
+                    val playlistId: String)
