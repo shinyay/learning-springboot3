5616b018b074f2ccf04902903342bb99d205a28a
shinyay <shinya.com@gmail.com>
2023-03-13 17:00:14 +0900

    Add methods for thumbnails

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchSnippet.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchSnippet.kt
@@ -7,4 +7,18 @@ data class SearchSnippet(
     val description: String,
     val thumbnails: Map<String, SearchThumbnail>,
     val channelTitle: String
-    )
+    ) {
+    fun shortDescription(): String? {
+        return if (description.length <= 100) {
+            description
+        } else description.substring(0, 100)
+    }
+
+    fun thumbnail_temp(): SearchThumbnail? {
+        return thumbnails.entries.stream()
+            .filter { entry -> entry.key.equals(("default")) }
+            .findFirst()
+            .map { it.value }
+            .orElse(null) as SearchThumbnail?
+    }
+}
