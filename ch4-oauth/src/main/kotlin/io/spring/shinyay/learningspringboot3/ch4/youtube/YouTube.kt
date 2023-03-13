74d418728d02432bec7d23676b2d03c249f4e5f3
shinyay <shinya.com@gmail.com>
2023-03-13 14:25:06 +0900

    Add YouTube interface using HTTP client proxy

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/YouTube.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/YouTube.kt
@@ -1,4 +1,22 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube
 
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.service.annotation.GetExchange
+
 interface YouTube {
+
+    @GetExchange("/search?part=snippet&type=video")
+    fun channelVideos( //
+        @RequestParam channelId: String?,  //
+        @RequestParam maxResults: Int,  //
+        @RequestParam order: Sort?
+    ): SearchListResponse?
+    
+    enum class Sort(private val type: String) {
+        DATE("date"),
+        VIEW_COUNT("viewCount"),
+        TITLE("title"),
+        RATING("rating")
+    }
+
 }
