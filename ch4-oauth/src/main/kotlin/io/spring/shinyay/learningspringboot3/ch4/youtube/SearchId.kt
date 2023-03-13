fabe834039c938ffb629a287ed99790b15e80768
shinyay <shinya.com@gmail.com>
2023-03-13 15:56:28 +0900

    Add fields for Search Id

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchId.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/SearchId.kt
@@ -1,3 +1,3 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube
 
-data class SearchId()
+data class SearchId(val kind: String, val videoId: String, val channelId: String, val playlistId: String)
