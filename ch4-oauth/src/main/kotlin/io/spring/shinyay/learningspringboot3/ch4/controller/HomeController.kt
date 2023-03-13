c9173fd43b30453b41f47372d4be4f422b001b1e
shinyay <shinya.com@gmail.com>
2023-03-13 16:47:12 +0900

    Add web page for video search

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/controller/HomeController.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/controller/HomeController.kt
@@ -1,7 +1,22 @@
 package io.spring.shinyay.learningspringboot3.ch4.controller
 
+import io.spring.shinyay.learningspringboot3.ch4.youtube.YouTube
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
 
 @Controller
-class HomeController {
+class HomeController(private val youTube: YouTube) {
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute(
+            "channelVideos",  //
+            youTube.channelVideos(
+                "UCjukbYOd6pjrMpNMFAOKYyw",  //
+                10, YouTube.Sort.VIEW_COUNT
+            )
+        )
+        return "index"
+    }
 }
