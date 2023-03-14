8cecdee0df1b486bdb390de93725e771aaf5d5cf
shinyay <shinya.com@gmail.com>
2023-03-14 17:23:26 +0900

    Add default value

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchThumbnail.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchThumbnail.kt
@@ -1,7 +1,8 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube.response
 
 data class SearchThumbnail(
-    val url: String,
-    val width: Int,
-    val height: Int
+    val url: String = "",
+    val width: Int = 0,
+    val height: Int = 0
 )
+
