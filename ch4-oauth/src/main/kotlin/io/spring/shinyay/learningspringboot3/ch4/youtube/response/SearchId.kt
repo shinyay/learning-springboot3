3de06c81a1f3c43efe0780d239b8732a0d4e8f80
shinyay <shinya.com@gmail.com>
2023-03-14 17:29:03 +0900

    Add default value

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchId.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchId.kt
@@ -1,6 +1,8 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube.response
 
-data class SearchId(val kind: String,
-                    val videoId: String,
-                    val channelId: String,
-                    val playlistId: String)
+data class SearchId(
+    val kind: String = "",
+    val videoId: String = "",
+    val channelId: String? = "",
+    val playlistId: String? = ""
+)
