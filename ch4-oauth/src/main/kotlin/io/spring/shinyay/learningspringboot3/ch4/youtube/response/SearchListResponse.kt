887d052155feaae9ba538dd487c7ae9b34dfa191
shinyay <shinya.com@gmail.com>
2023-03-14 17:20:12 +0900

    Add default value

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchListResponse.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchListResponse.kt
@@ -1,10 +1,10 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube.response
 
 data class SearchListResponse(
-    val kind: String,
-    val etag: String,
-    val nextPageToken: String,
-    val prevPageToken: String,
-    val pageInfo: PageInfo,
-    val items: Array<SearchResult>
+    val kind: String = "",
+    val etag: String = "",
+    val nextPageToken: String = "",
+    val prevPageToken: String? = "",
+    val pageInfo: PageInfo? = null,
+    val items: Array<SearchResult>? = null
 )
