6dcef28a8cf96f136a346263170f942eaa2173fc
shinyay <shinya.com@gmail.com>
2023-03-14 17:26:55 +0900

    Add default value

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchSnippet.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/SearchSnippet.kt
@@ -1,24 +1,28 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube.response
 
+import java.util.function.Function
+
+
+@JvmRecord
 data class SearchSnippet(
-    val publishedAt: String,
-    val channelId: String,
-    val title: String,
-    val description: String,
-    val thumbnails: Map<String, SearchThumbnail>,
-    val channelTitle: String
-    ) {
-    fun shortDescription(): String? {
+    val publishedAt: String = "",
+    val channelId: String = "",
+    val title: String = "",
+    val description: String = "",
+    val thumbnails: Map<String, SearchThumbnail>? = null,
+    val channelTitle: String = ""
+) {
+    fun shortDescription(): String {
         return if (description.length <= 100) {
             description
         } else description.substring(0, 100)
     }
 
-    fun thumbnail_temp(): SearchThumbnail? {
-        return thumbnails.entries.stream()
-            .filter { entry -> entry.key.equals(("default")) }
-            .findFirst()
-            .map { it.value }
-            .orElse(null) as SearchThumbnail?
+    fun thumbnail(): SearchThumbnail? {
+        return thumbnails!!.entries.stream() //
+            .filter { (key): Map.Entry<String, SearchThumbnail> -> key == "default" } //
+            .findFirst() //
+            .map { it.value } //
+            .orElse(null)
     }
 }
