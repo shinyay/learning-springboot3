d2113e3701f23e2aa5db265b74f0fe83b1640e95
shinyay <shinya.com@gmail.com>
2023-03-14 17:21:32 +0900

    Add default value

--- ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/PageInfo.kt
+++ ch4-oauth/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/youtube/response/PageInfo.kt
@@ -1,4 +1,6 @@
 package io.spring.shinyay.learningspringboot3.ch4.youtube.response
 
-data class PageInfo(val totalResults: Int,
-                    val resultPerPage: Int)
+data class PageInfo(
+    val totalResults: Int = 0,
+    val resultsPerPage: Int = 0)
+
