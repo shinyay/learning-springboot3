df7beeb8fd9f708413cea56f42d3a751886b9eca
shinyay <shinya.com@gmail.com>
2023-03-15 19:03:35 +0900

    Add Chapter 5 template project duplicated from Chapter 4

---
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package io.spring.shinyay.learningspringboot3.ch4.security.repository
+
+import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
+import org.springframework.data.repository.Repository
+
+interface UserRepository : Repository<UserAccount, Long> {
+
+    fun findByUsername(username: String): UserAccount
+}
---
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/video/controller/HomeController.kt
@@ -0,0 +1,43 @@
+package io.spring.shinyay.learningspringboot3.ch4.video.controller
+
+import io.spring.shinyay.learningspringboot3.ch4.video.dto.UniversalSearch
+import io.spring.shinyay.learningspringboot3.ch4.video.dto.VideoSearch
+import io.spring.shinyay.learningspringboot3.ch4.video.dto.NewVideo
+import io.spring.shinyay.learningspringboot3.ch4.video.entity.VideoEntity
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import io.spring.shinyay.learningspringboot3.ch4.video.service.VideoService
+
+@Controller
+class HomeController(val videoService: VideoService) {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("videos", videoService.getVideos())
+        return "index"
+    }
+
+    @PostMapping("/new-video")
+    fun newVideo(@ModelAttribute video: NewVideo): String {
+        videoService.create(video)
+        return "redirect:/"
+    }
+
+//    @PostMapping("/multi-field-search")
+//    fun multiFieldSearch(@ModelAttribute search: VideoSearch, model: Model): String {
+//        val searchResult: List<VideoEntity>? = videoService.search(search)
+//        model.addAttribute("videos", searchResult)
+//        return "index"
+//    }
+
+//    @PostMapping("/universal-search")
+    @PostMapping("/search")
+    fun universalSearch(@ModelAttribute search: UniversalSearch, model: Model): String {
+        val searchResults = videoService.search(search)
+        model.addAttribute("videos", searchResults)
+        return "index"
+    }
+}
