fcf0905a08fed83453a7ab34d0f164f9147a4240
shinyay <shinya.com@gmail.com>
2023-03-16 18:10:46 +0900

    Modify code to clean up

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserManagementRepository.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch4/security/repository/UserManagementRepository.kt
@@ -3,5 +3,4 @@ package io.spring.shinyay.learningspringboot3.ch4.security.repository
 import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserManagementRepository : JpaRepository<UserAccount, Long> {
-}
+interface UserManagementRepository : JpaRepository<UserAccount, Long>
