67f0beb267aa6019027494d26e8b519febbe9a2e
shinyay <shinya.com@gmail.com>
2023-03-18 17:47:06 +0900

    Add test for VideEntity

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch4/CoreDomainTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch4/CoreDomainTest.kt
@@ -1,13 +1,22 @@
 package io.spring.shinyay.learningspringboot3.ch4
 
+import io.spring.shinyay.learningspringboot3.ch5.video.entity.VideoEntity
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+
 
 @SpringBootTest
 class CoreDomainTest {
 
 	@Test
-	fun contextLoads() {
+	fun newVideoEntityShouldHaveNullId() {
+		val entity = VideoEntity("alice", "title", "description")
+		assertThat(entity.id).isNull()
+		assertThat(entity.username).isEqualTo("alice")
+		assertThat(entity.name).isEqualTo("title")
+		assertThat(entity.description) //
+			.isEqualTo("description")
 	}
 
 }
