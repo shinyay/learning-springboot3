1d633a3df533a088783750dbcf670ebd8d4f9483
shinyay <shinya.com@gmail.com>
2023-03-18 17:35:53 +0900

    Midify package name for cf5

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/Chapter4Application.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/Chapter4Application.kt
@@ -1,4 +1,4 @@
-package io.spring.shinyay.learningspringboot3.ch4
+package io.spring.shinyay.learningspringboot3.ch5
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
