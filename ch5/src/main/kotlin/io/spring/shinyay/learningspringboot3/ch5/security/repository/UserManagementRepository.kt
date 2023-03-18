e0582d4a3981c337e9ccc18354a36efc1e9f6367
shinyay <shinya.com@gmail.com>
2023-03-18 17:36:41 +0900

    Modify package name for Chapter 5

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/security/repository/UserManagementRepository.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/security/repository/UserManagementRepository.kt
@@ -1,6 +1,6 @@
-package io.spring.shinyay.learningspringboot3.ch4.security.repository
+package io.spring.shinyay.learningspringboot3.ch5.security.repository
 
-import io.spring.shinyay.learningspringboot3.ch4.security.entity.UserAccount
+import io.spring.shinyay.learningspringboot3.ch5.security.entity.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserManagementRepository : JpaRepository<UserAccount, Long>
