d2b35cb2e5f531488bf892afced4921ad587bac3
shinyay <shinya.com@gmail.com>
2023-03-18 17:37:13 +0900

    Modify package name for Chapter 5

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/dto/VideoSearch.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/dto/VideoSearch.kt
@@ -1,3 +1,3 @@
-package io.spring.shinyay.learningspringboot3.ch4.video.dto
+package io.spring.shinyay.learningspringboot3.ch5.video.dto
 
 data class VideoSearch(val name: String, val description: String)
