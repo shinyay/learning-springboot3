acbabd8e2e7cb0eaa84e17053b5dc89eb1dbe403
shinyay <shinya.com@gmail.com>
2023-03-22 17:33:10 +0900

    Add toStpring genereted by InteliJ

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/entity/VideoEntity.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/entity/VideoEntity.kt
@@ -12,4 +12,8 @@ class VideoEntity(var username: String?, var name: String?, var description: Str
     var id: Long? = null
 
     constructor() : this(null, null, null)
+
+    override fun toString(): String {
+        return "VideoEntity(username=$username, name=$name, description=$description, id=$id)"
+    }
 }
