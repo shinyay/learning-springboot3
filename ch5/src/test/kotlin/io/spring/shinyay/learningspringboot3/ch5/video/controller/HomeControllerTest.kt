8a74c294f67a9fefd8ec67c7d26e03ad774b566d
shinyay <shinya.com@gmail.com>
2023-03-23 18:25:15 +0900

    Add get url with MockMvcRequestBuilder

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 
 @WebMvcTest(controllers = [HomeController::class])
@@ -20,6 +21,8 @@ class HomeControllerTest {
 
     @Test
     fun indexPageHasSeveralHtmlFroms() {
-        
+        val html = mvc.perform(
+            get("/")
+        )
     }
 }
