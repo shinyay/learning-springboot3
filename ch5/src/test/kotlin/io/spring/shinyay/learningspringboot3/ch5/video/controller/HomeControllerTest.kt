f410c71928f81ef34b8c3d5ec5e2e039699bc92f
shinyay <shinya.com@gmail.com>
2023-03-23 18:27:17 +0900

    Add result matcher

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 
 @WebMvcTest(controllers = [HomeController::class])
@@ -23,6 +24,8 @@ class HomeControllerTest {
     fun indexPageHasSeveralHtmlFroms() {
         val html = mvc.perform(
             get("/")
+        ).andExpect(
+            status().isOk
         )
     }
 }
