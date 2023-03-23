3014efb8df29a045214599db87ac4851a64ed804
shinyay <shinya.com@gmail.com>
2023-03-23 15:28:10 +0900

    Add WebMvcTest annotation for testing Controller

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
@@ -1,4 +1,14 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
+import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.MockMvc
+
+
+@WebMvcTest(controllers = [HomeController::class])
 class HomeControllerTest {
+
 }
