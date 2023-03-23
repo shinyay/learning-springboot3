01107be2f56883732340989563db489f74363daa
shinyay <shinya.com@gmail.com>
2023-03-23 18:29:39 +0900

    Add hamcrest matcher

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
@@ -1,6 +1,7 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
 import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
+import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -8,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 
@@ -26,6 +28,10 @@ class HomeControllerTest {
             get("/")
         ).andExpect(
             status().isOk
+        ).andExpect(
+            content().string(
+                containsString("Username: user")
+            )
         )
     }
 }
