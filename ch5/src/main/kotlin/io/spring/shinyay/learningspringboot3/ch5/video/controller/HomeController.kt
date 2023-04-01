887914d75452a61ce9263a7e79d97848c3afd11c
shinyay <shinya.com@gmail.com>
2023-04-01 09:31:29 +0900

    Add delete function

--- ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeController.kt
+++ ch5/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeController.kt
@@ -1,14 +1,16 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
 import io.spring.shinyay.learningspringboot3.ch5.video.dto.NewVideo
-import io.spring.shinyay.learningspringboot3.ch5.video.dto.UniversalSearch
+import io.spring.shinyay.learningspringboot3.ch5.video.dto.Search
 import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+
 
 @Controller
 class HomeController(val videoService: VideoService) {
@@ -32,5 +34,11 @@ class HomeController(val videoService: VideoService) {
         val searchResults = videoService.search(search)
         model.addAttribute("videos", searchResults)
         return "index"
+    }
+
+    @PostMapping("/delete/videos/{videoId}")
+    fun deleteVideo(@PathVariable videoId: Long): String? {
+        videoService.delete(videoId)
+        return "redirect:/"
     }
 }
