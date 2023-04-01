e171b4c6e31c21a2c7af645d1a5e90b0221e64a7
shinyay <shinya.com@gmail.com>
2023-04-01 13:25:21 +0900

    Add Given When Then test

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
@@ -1,10 +1,15 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.service
 
+import io.spring.shinyay.learningspringboot3.ch5.video.entity.VideoEntity
 import io.spring.shinyay.learningspringboot3.ch5.video.repository.VideoRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.List
 
 
 @ExtendWith(MockitoExtension::class)
@@ -18,5 +23,19 @@ class VideoServiceTest {
     @BeforeEach
     fun setUp() {
         service = VideoService(repository)
+    }
+
+    @Test
+    fun getVideosShouldReturnAll() {
+        // given
+        val video1 = VideoEntity("alice", "Spring Boot 3 Intro", "Learn the basics!")
+        val video2 = VideoEntity("alice", "Spring Boot 3 Deep Dive", "Go deep!")
+        `when`(repository.findAll()).thenReturn(listOf(video1, video2))
+
+        // when
+        val videos = service.getVideos()
+
+        // then
+        assertThat(videos).containsExactly(video1, video2)
     }
 }
