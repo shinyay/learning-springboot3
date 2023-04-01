d87401ed75299894b0d8d69374a82bcae58f93b5
shinyay <shinya.com@gmail.com>
2023-04-01 13:15:51 +0900

    Add Mock Extension for Mock annotation

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
@@ -1,4 +1,8 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.service
 
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
 class VideoServiceTest {
 }
