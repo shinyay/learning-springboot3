65076cb024b8272486fbc12e13c66664e57f1aba
shinyay <shinya.com@gmail.com>
2023-04-01 13:18:55 +0900

    Add repository as mock

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/service/VideoServiceTest.kt
@@ -1,8 +1,22 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.service
 
+import io.spring.shinyay.learningspringboot3.ch5.video.repository.VideoRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+
 
 @ExtendWith(MockitoExtension::class)
 class VideoServiceTest {
+
+    lateinit var service: VideoService
+
+    @Mock
+    lateinit var repository: VideoRepository
+
+    @BeforeEach
+    fun setUp() {
+        service = VideoService(repository)
+    }
 }
