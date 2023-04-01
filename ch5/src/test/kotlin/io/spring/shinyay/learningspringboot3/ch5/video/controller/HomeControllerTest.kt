f501b58b28cde54b78af08aa869ec3d4366b53bf
shinyay <shinya.com@gmail.com>
2023-04-01 09:46:18 +0900

    Add test for post to the form

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/HomeControllerTest.kt
@@ -1,17 +1,20 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
+import io.spring.shinyay.learningspringboot3.ch5.video.dto.NewVideo
 import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
 import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 
 @WebMvcTest(controllers = [HomeController::class])
@@ -58,6 +61,26 @@ class HomeControllerTest {
             "<form action=\"/logout\"",
             "<form action=\"/search\"",
             "<form action=\"/new-video\""
+        )
+    }
+
+    @Test
+    @WithMockUser
+    @Throws(Exception::class)
+    fun postNewVideoShouldWork() {
+        mvc.perform(
+            post("/new-video")
+                .param("name", "new video")
+                .param("description", "new description")
+                .with(csrf())
+        )
+            .andExpect(redirectedUrl("/"))
+        verify(videoService).create(
+            NewVideo(
+                "new video",
+                "new description"
+            ),
+            "user"
         )
     }
 }
