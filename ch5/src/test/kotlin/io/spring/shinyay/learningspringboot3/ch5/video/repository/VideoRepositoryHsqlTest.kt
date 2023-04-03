7b8526fd49c3c7bfa6e0592ab18051212d9312c3
shinyay <shinya.com@gmail.com>
2023-04-03 15:46:53 +0900

    Add repository as constructor injection

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryHsqlTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryHsqlTest.kt
@@ -3,5 +3,6 @@ package io.spring.shinyay.learningspringboot3.ch5.video.repository
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
-class VideoRepositoryHsqlTest {
+class VideoRepositoryHsqlTest(val repository: VideoRepository) {
+    
 }
