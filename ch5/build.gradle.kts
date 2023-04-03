575c73cacfa4db4e4a969613f1b5c29e0cb7073b
shinyay <shinya.com@gmail.com>
2023-04-03 15:30:05 +0900

    Add HyperSQL Database, hsqldb

--- ch5/build.gradle.kts
+++ ch5/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.hsqldb:hsqldb")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
