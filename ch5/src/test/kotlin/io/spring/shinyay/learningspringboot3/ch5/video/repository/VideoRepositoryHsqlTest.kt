11d11225d009fa23243a57b335bce8c759ca3779
shinyay <shinya.com@gmail.com>
2023-04-03 15:48:50 +0900

    Add initial data for testing

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryHsqlTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryHsqlTest.kt
@@ -1,8 +1,33 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.repository
 
+import io.spring.shinyay.learningspringboot3.ch5.video.entity.VideoEntity
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
 
 @DataJpaTest
 class VideoRepositoryHsqlTest(val repository: VideoRepository) {
-    
+
+    @BeforeEach
+    fun setUp() {
+        repository.saveAll(
+            mutableListOf(
+                VideoEntity(
+                    "alice",
+                    "Need HELP with your SPRING BOOT 3 App?",
+                    "SPRING BOOT 3 will only speed things up."
+                ),
+                VideoEntity(
+                    "alice",
+                    "Don't do THIS to your own CODE!",
+                    "As a pro developer, never ever EVER do this to your code."
+                ),
+                VideoEntity(
+                    "bob",
+                    "SECRETS to fix BROKEN CODE!",
+                    "Discover ways to not only debug your code"
+                )
+            )
+        )
+    }
 }
