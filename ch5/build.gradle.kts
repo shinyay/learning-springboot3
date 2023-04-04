cb36326ab5b51de4ecbc0d90f7edae1e6e25b06b
shinyay <shinya.com@gmail.com>
2023-04-04 16:28:06 +0900

    Midify testcontainer version as 1.17.6

--- ch5/build.gradle.kts
+++ ch5/build.gradle.kts
@@ -15,7 +15,8 @@ repositories {
 	mavenCentral()
 }
 
-extra["testcontainersVersion"] = "1.18.0"
+//extra["testcontainersVersion"] = "1.18.0"
+extra["testcontainersVersion"] = "1.17.6"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
