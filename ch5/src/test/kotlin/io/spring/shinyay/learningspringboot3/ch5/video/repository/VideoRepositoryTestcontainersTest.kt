e6802cb8cfeac2eeed2196cf0ebf4d5e5a022559
shinyay <shinya.com@gmail.com>
2023-04-04 16:32:43 +0900

    Add AutoConfigureTestDatabase not to replace Dataseource Bean

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
@@ -1,9 +1,11 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.repository
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VideoRepositoryTestcontainersTest {
 }
