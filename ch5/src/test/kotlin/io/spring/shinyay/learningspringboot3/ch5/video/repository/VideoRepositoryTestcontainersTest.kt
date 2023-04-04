3d1bb27ff57443243281c119295cc70c25d1b997
shinyay <shinya.com@gmail.com>
2023-04-04 16:28:31 +0900

    Add testcontainer annotation

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
@@ -1,7 +1,9 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.repository
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @DataJpaTest
 class VideoRepositoryTestcontainersTest {
 }
