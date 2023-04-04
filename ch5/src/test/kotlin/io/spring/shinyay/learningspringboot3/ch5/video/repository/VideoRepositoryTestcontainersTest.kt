8af0ed8573d90343d48263fd9ea6203c49d7263f
shinyay <shinya.com@gmail.com>
2023-04-04 16:44:20 +0900

    Add PostgreSQL container

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/repository/VideoRepositoryTestcontainersTest.kt
@@ -1,11 +1,23 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.repository
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+
 
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class VideoRepositoryTestcontainersTest {
+class VideoRepositoryTestcontainersTest(
+    @Autowired
+    val repository: VideoRepository
+) {
+    @Container
+    val database = PostgreSQLContainer<Nothing>("postgres:9.6.12").apply {
+        withUsername("postgres")
+    }
+
 }
