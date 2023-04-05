7aec4620ac7ee4e945d7429245552e5bb8959540
shinyay <shinya.com@gmail.com>
2023-04-05 15:41:39 +0900

    Add constructor and Autowired annotation to enable multiple fields

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/SecurityBasedTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/SecurityBasedTest.kt
@@ -1,13 +1,18 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
 import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @WebMvcTest(controllers = [HomeController::class])
-class SecurityBasedTest(
-    @Autowired val mockMvc: MockMvc,
+class SecurityBasedTest @Autowired constructor(
+    private val mockMvc: MockMvc,
     @MockBean val videoService: VideoService) {
+
+
 }
