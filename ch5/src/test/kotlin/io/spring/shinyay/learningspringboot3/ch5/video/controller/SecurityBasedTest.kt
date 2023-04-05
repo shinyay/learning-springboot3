1cfd3a4f1ce0a0a973447f1a7ff7b905e3efd61f
shinyay <shinya.com@gmail.com>
2023-04-05 15:04:02 +0900

    Add MockMVC to enable Mock and VideoService as Mock

--- ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/SecurityBasedTest.kt
+++ ch5/src/test/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/controller/SecurityBasedTest.kt
@@ -1,4 +1,13 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.controller
 
-class SecurityBasedTest {
+import io.spring.shinyay.learningspringboot3.ch5.video.service.VideoService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@WebMvcTest(controllers = [HomeController::class])
+class SecurityBasedTest(
+    @Autowired val mockMvc: MockMvc,
+    @MockBean val videoService: VideoService) {
 }
