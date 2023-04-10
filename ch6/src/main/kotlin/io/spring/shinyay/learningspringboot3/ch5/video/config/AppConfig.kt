5a7e40c3c9bbfe70968620a23c863dc3a30ce9cf
shinyay <shinya.com@gmail.com>
2023-04-10 09:43:30 +0900

    Modify the class to Data Class with prioerties

--- ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/config/AppConfig.kt
+++ ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch5/video/config/AppConfig.kt
@@ -1,4 +1,9 @@
 package io.spring.shinyay.learningspringboot3.ch5.video.config
 
-class AppConfig {
-}
+import io.spring.shinyay.learningspringboot3.ch5.security.entity.UserAccount
+
+data class AppConfig (
+    val header: String,
+    val intro: String,
+    val list: MutableList<UserAccount>
+)
