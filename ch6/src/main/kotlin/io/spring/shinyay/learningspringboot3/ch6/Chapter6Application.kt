2924d21539ef40058d90fc1b4289409f15ae872c
shinyay <shinya.com@gmail.com>
2023-04-10 09:54:07 +0900

    Add EnableConfigurationProperties annotation to enable AppConfig to the Customn properties

--- ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch6/Chapter6Application.kt
+++ ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch6/Chapter6Application.kt
@@ -1,9 +1,12 @@
 package io.spring.shinyay.learningspringboot3.ch6
 
+import io.spring.shinyay.learningspringboot3.ch6.video.config.AppConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppConfig::class)
 class Chapter6Application
 
 fun main(args: Array<String>) {
