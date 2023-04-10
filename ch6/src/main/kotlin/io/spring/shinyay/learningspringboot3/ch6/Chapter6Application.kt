21f6857bd501e4f11689a5d06c974a6dd6991be6
shinyay <shinya.com@gmail.com>
2023-04-10 09:50:03 +0900

    Modify package name for Chapter 6

--- ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch6/Chapter6Application.kt
+++ ch6/src/main/kotlin/io/spring/shinyay/learningspringboot3/ch6/Chapter6Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Chapter4Application
+class Chapter6Application
 
 fun main(args: Array<String>) {
-	runApplication<Chapter4Application>(*args)
+	runApplication<Chapter6Application>(*args)
 }
